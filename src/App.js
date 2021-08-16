c230bd8e06628bc2963453b1f735113ade6ba5ef
Sean Moriarty <metalfacesec@gmail.com>
2021-08-15 20:20:30 -0400

    Adding sub text to header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Header from './components/Header';
-import Contact from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Contact></Contact>
     </div>
   );
 }
