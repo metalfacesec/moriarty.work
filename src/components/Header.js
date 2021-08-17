201743e9d726fd4ebd0d545477ffb078e5225f63
Sean Moriarty <metalfacesec@gmail.com>
2021-08-16 21:50:32 -0400

    Fixing padding on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,13 +15,11 @@ class Header extends React.Component {
                         <div className="header-name">Sean Moriarty</div>
                         <div className="header-sub-text">Maker / Developer</div>
                         <Row>
-                            <Col sm={0} md={3}></Col>
-                            <Col sm={12} md={6}>
-                                <div className="header-about-me-text">
+                            <Col sm={0} md={4}></Col>
+                            <Col sm={12} md={4} className="header-about-me-text">
                                     In my professional life I spend most of my time working with PHP and Javascript. I also work with Docker, AWS, MySQL, NoSQL, Redis and many other technologies and frameworks.
                                     In my free time I can commonly be found fixing and restoring arcade machines and game consoles. I'm also know to work with 3D modeling and 3D printing. I love taking things apart
                                     and figuring out how they work, as well as coming up with my own improvements and solutions to problems with products I own.
-                                </div>
                             </Col>
                         </Row>
                     </Col>
